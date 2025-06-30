e5fcb4ebe63f0b7ce2580efa2acf31fe929d54f7
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-07-01 03:50:06 +0530

    added schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,5 +1,5 @@
 import { is, relations, sql } from "drizzle-orm";
-import { index, pgTableCreator, primaryKey } from "drizzle-orm/pg-core";
+import { index, pgTableCreator, primaryKey, PgArray } from "drizzle-orm/pg-core";
 import { type AdapterAccount } from "next-auth/adapters";
 import { receiveMessageOnPort } from "worker_threads";
 
@@ -11,8 +11,6 @@ import { receiveMessageOnPort } from "worker_threads";
  */
 export const createTable = pgTableCreator((name) => `BusMate_${name}`);
 
-
-
 export const users = createTable("user", (d) => ({
   id: d
     .varchar({ length: 255 })
@@ -20,20 +18,17 @@ export const users = createTable("user", (d) => ({
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
 
-  
   rollNo: d.varchar({ length: 10 }).notNull().unique(),
   name: d.varchar({ length: 255 }),
   gender: d.varchar({ length: 255 }).notNull(),
   email: d.varchar({ length: 255 }).notNull().unique(),
-  phone: d
-    .varchar({ length: 15 })
-    .notNull(),
+  phone: d.varchar({ length: 15 }).notNull(),
   address: d.varchar({ length: 255 }).notNull(),
   dateOfBirth: d
     .timestamp({ mode: "date", withTimezone: true })
     .notNull()
     .$defaultFn(() => sql`now()`),
-  
+
   busId: d.varchar({ length: 255 }).notNull(),
   seatId: d.varchar({ length: 255 }).notNull(),
   receiptId: d.varchar({ length: 255 }).notNull(),
@@ -63,20 +58,45 @@ export const accounts = createTable(
       .notNull()
       .references(() => users.id),
     password: d.varchar({ length: 255 }).notNull(),
-    }),
-  (t) => [
-    index("account_user_id_idx").on(t.userId),
-  ],
+  }),
+  (t) => [index("account_user_id_idx").on(t.userId)],
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
   user: one(users, { fields: [accounts.userId], references: [users.id] }),
 }));
 
-export const acceptedRolls = createTable(
-  "acceptedRolls",
-  (d) => ({
-      rollNo: d.varchar({ length: 10 }).notNull().unique(),
-      boardingPoint: d.varchar({ length: 255 }).notNull(),
-  })
-)
+export const acceptedRolls = createTable("acceptedRolls", (d) => ({
+  rollNo: d.varchar({ length: 10 }).notNull().unique(),
+  boardingPoint: d.varchar({ length: 255 }).notNull(),
+}));
+
+export const buses = createTable("bus", (d) => ({
+  id: d
+    .varchar({ length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  busNumber: d.varchar({ length: 255 }).notNull().unique(),
+  model: d.varchar({ length: 255 }).notNull(),
+  routeName: d.varchar({ length: 255 }).notNull(),
+  boardingPoints: d.PgArray(d.varchar({ length: 255 })).notNull(),
+  driverName: d.varchar({ length: 255 }).notNull(),
+  driverPhone: d.varchar({ length: 15 }).notNull(),
+  createdAt: d
+    .timestamp({ mode: "date", withTimezone: true })
+    .notNull()
+    .$defaultFn(() => sql`now()`),
+  updatedAt: d
+    .timestamp({ mode: "date", withTimezone: true })
+    .notNull()
+    .$defaultFn(() => sql`now()`),
+}));
+
+export const seats = createTable("seat", (d) => ({
+  id: d.varchar({ length: 255 }).notNull().primaryKey().$defaultFn(() => crypto.randomUUID()),
+  busId: d.varchar({ length: 255 }).notNull().references(() => buses.id),
+  userId: d.varchar({ length: 255 }),
+  satus: d
+    .enum("available", "booked", "reserved")
+}));
