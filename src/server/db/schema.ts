250a4610e2ccb5668a896c36b1e82df7f01eaf60
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-07-01 03:38:03 +0530

    added schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,7 @@
-import { relations, sql } from "drizzle-orm";
+import { is, relations, sql } from "drizzle-orm";
 import { index, pgTableCreator, primaryKey } from "drizzle-orm/pg-core";
 import { type AdapterAccount } from "next-auth/adapters";
+import { receiveMessageOnPort } from "worker_threads";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -10,26 +11,7 @@ import { type AdapterAccount } from "next-auth/adapters";
  */
 export const createTable = pgTableCreator((name) => `BusMate_${name}`);
 
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdById: d
-      .varchar({ length: 255 })
-      .notNull()
-      .references(() => users.id),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [
-    index("created_by_idx").on(t.createdById),
-    index("name_idx").on(t.name),
-  ],
-);
+
 
 export const users = createTable("user", (d) => ({
   id: d
@@ -37,15 +19,36 @@ export const users = createTable("user", (d) => ({
     .notNull()
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
+
+  
+  rollNo: d.varchar({ length: 10 }).notNull().unique(),
   name: d.varchar({ length: 255 }),
-  email: d.varchar({ length: 255 }).notNull(),
-  emailVerified: d
-    .timestamp({
-      mode: "date",
-      withTimezone: true,
-    })
-    .default(sql`CURRENT_TIMESTAMP`),
-  image: d.varchar({ length: 255 }),
+  gender: d.varchar({ length: 255 }).notNull(),
+  email: d.varchar({ length: 255 }).notNull().unique(),
+  phone: d
+    .varchar({ length: 15 })
+    .notNull(),
+  address: d.varchar({ length: 255 }).notNull(),
+  dateOfBirth: d
+    .timestamp({ mode: "date", withTimezone: true })
+    .notNull()
+    .$defaultFn(() => sql`now()`),
+  
+  busId: d.varchar({ length: 255 }).notNull(),
+  seatId: d.varchar({ length: 255 }).notNull(),
+  receiptId: d.varchar({ length: 255 }).notNull(),
+  isVerified: d.boolean().notNull().default(false),
+
+  isAdmin: d.boolean().notNull().default(false),
+
+  createdAt: d
+    .timestamp({ mode: "date", withTimezone: true })
+    .notNull()
+    .$defaultFn(() => sql`now()`),
+  updatedAt: d
+    .timestamp({ mode: "date", withTimezone: true })
+    .notNull()
+    .$defaultFn(() => sql`now()`),
 }));
 
 export const usersRelations = relations(users, ({ many }) => ({
@@ -59,19 +62,9 @@ export const accounts = createTable(
       .varchar({ length: 255 })
       .notNull()
       .references(() => users.id),
-    type: d.varchar({ length: 255 }).$type<AdapterAccount["type"]>().notNull(),
-    provider: d.varchar({ length: 255 }).notNull(),
-    providerAccountId: d.varchar({ length: 255 }).notNull(),
-    refresh_token: d.text(),
-    access_token: d.text(),
-    expires_at: d.integer(),
-    token_type: d.varchar({ length: 255 }),
-    scope: d.varchar({ length: 255 }),
-    id_token: d.text(),
-    session_state: d.varchar({ length: 255 }),
-  }),
+    password: d.varchar({ length: 255 }).notNull(),
+    }),
   (t) => [
-    primaryKey({ columns: [t.provider, t.providerAccountId] }),
     index("account_user_id_idx").on(t.userId),
   ],
 );
@@ -80,29 +73,10 @@ export const accountsRelations = relations(accounts, ({ one }) => ({
   user: one(users, { fields: [accounts.userId], references: [users.id] }),
 }));
 
-export const sessions = createTable(
-  "session",
+export const acceptedRolls = createTable(
+  "acceptedRolls",
   (d) => ({
-    sessionToken: d.varchar({ length: 255 }).notNull().primaryKey(),
-    userId: d
-      .varchar({ length: 255 })
-      .notNull()
-      .references(() => users.id),
-    expires: d.timestamp({ mode: "date", withTimezone: true }).notNull(),
-  }),
-  (t) => [index("t_user_id_idx").on(t.userId)],
-);
-
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
-}));
-
-export const verificationTokens = createTable(
-  "verification_token",
-  (d) => ({
-    identifier: d.varchar({ length: 255 }).notNull(),
-    token: d.varchar({ length: 255 }).notNull(),
-    expires: d.timestamp({ mode: "date", withTimezone: true }).notNull(),
-  }),
-  (t) => [primaryKey({ columns: [t.identifier, t.token] })],
-);
+      rollNo: d.varchar({ length: 10 }).notNull().unique(),
+      boardingPoint: d.varchar({ length: 255 }).notNull(),
+  })
+)
