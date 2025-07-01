0412c287aa2254ec150a203ecf2befa71562c6e4
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-07-02 01:03:51 +0530

    update schema-1

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,16 +1,11 @@
-import { is, relations, sql } from "drizzle-orm";
-import { index, pgTableCreator, primaryKey, PgArray } from "drizzle-orm/pg-core";
-import { type AdapterAccount } from "next-auth/adapters";
-import { receiveMessageOnPort } from "worker_threads";
+import { relations, sql } from "drizzle-orm";
+import { index, pgTableCreator, primaryKey, pgEnum } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
 export const createTable = pgTableCreator((name) => `BusMate_${name}`);
 
+export const genderEnum = pgEnum("genderEnum", ["male", "female"]);
+
+//user table
 export const users = createTable("user", (d) => ({
   id: d
     .varchar({ length: 255 })
@@ -19,18 +14,22 @@ export const users = createTable("user", (d) => ({
     .$defaultFn(() => crypto.randomUUID()),
 
   rollNo: d.varchar({ length: 10 }).notNull().unique(),
-  name: d.varchar({ length: 255 }),
-  gender: d.varchar({ length: 255 }).notNull(),
+  name: d.varchar({ length: 255 }).notNull(),
+  gender: genderEnum("gender").notNull(),
   email: d.varchar({ length: 255 }).notNull().unique(),
   phone: d.varchar({ length: 15 }).notNull(),
   address: d.varchar({ length: 255 }).notNull(),
-  dateOfBirth: d
-    .timestamp({ mode: "date", withTimezone: true })
-    .notNull()
-    .$defaultFn(() => sql`now()`),
+  dateOfBirth: d.timestamp({ mode: "date", withTimezone: true }).notNull(),
 
-  busId: d.varchar({ length: 255 }).notNull(),
+  busId: d
+    .varchar({ length: 255 })
+    .references(() => buses.id)
+    .notNull(),
   seatId: d.varchar({ length: 255 }).notNull(),
+  boardingPointId: d
+    .varchar({ length: 255 })
+    .notNull()
+    .references(() => boardingPoints.id),
   receiptId: d.varchar({ length: 255 }).notNull(),
   isVerified: d.boolean().notNull().default(false),
 
@@ -46,20 +45,32 @@ export const users = createTable("user", (d) => ({
     .$defaultFn(() => sql`now()`),
 }));
 
-export const usersRelations = relations(users, ({ many }) => ({
-  accounts: many(accounts),
+export const usersRollEmailBusIdx = index("users_roll_email_bus_idx").on(
+  users.rollNo,
+  users.email,
+  users.busId,
+);
+
+export const usersRelations = relations(users, ({ one }) => ({
+  bus: one(buses, { fields: [users.busId], references: [buses.id] }),
+  seat: one(seats, { fields: [users.seatId], references: [seats.id] }),
+  boardingPoint: one(boardingPoints, {
+    fields: [users.boardingPointId],
+    references: [boardingPoints.id],
+  }),
 }));
 
-export const accounts = createTable(
-  "account",
-  (d) => ({
-    userId: d
-      .varchar({ length: 255 })
-      .notNull()
-      .references(() => users.id),
-    password: d.varchar({ length: 255 }).notNull(),
-  }),
-  (t) => [index("account_user_id_idx").on(t.userId)],
+export const accounts = createTable("account", (d) => ({
+  userId: d
+    .varchar({ length: 255 })
+    .notNull()
+    .references(() => users.id)
+    .primaryKey(),
+  password: d.varchar({ length: 255 }).notNull(),
+}));
+
+export const accountsUserIdIdx = index("accounts_user_id_idx").on(
+  accounts.userId,
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -68,7 +79,19 @@ export const accountsRelations = relations(accounts, ({ one }) => ({
 
 export const acceptedRolls = createTable("acceptedRolls", (d) => ({
   rollNo: d.varchar({ length: 10 }).notNull().unique(),
-  boardingPoint: d.varchar({ length: 255 }).notNull(),
+  boardingPointId: d
+    .varchar({ length: 255 })
+    .notNull()
+    .references(() => boardingPoints.id),
+}));
+
+export const rollNoIdx = index("accepted_roll_no_idx").on(acceptedRolls.rollNo);
+
+export const acceptedRollsRelations = relations(acceptedRolls, ({ one }) => ({
+  boardingPoint: one(boardingPoints, {
+    fields: [acceptedRolls.boardingPointId],
+    references: [boardingPoints.id],
+  }),
 }));
 
 export const buses = createTable("bus", (d) => ({
@@ -77,12 +100,14 @@ export const buses = createTable("bus", (d) => ({
     .notNull()
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
-  busNumber: d.varchar({ length: 255 }).notNull().unique(),
+
   model: d.varchar({ length: 255 }).notNull(),
-  routeName: d.varchar({ length: 255 }).notNull(),
-  boardingPoints: d.PgArray(d.varchar({ length: 255 })).notNull(),
+
+  busNumber: d.varchar({ length: 10 }).notNull().unique(),
+  routeName: d.varchar({ length: 255 }),
   driverName: d.varchar({ length: 255 }).notNull(),
   driverPhone: d.varchar({ length: 15 }).notNull(),
+
   createdAt: d
     .timestamp({ mode: "date", withTimezone: true })
     .notNull()
@@ -93,10 +118,90 @@ export const buses = createTable("bus", (d) => ({
     .$defaultFn(() => sql`now()`),
 }));
 
-export const seats = createTable("seat", (d) => ({
-  id: d.varchar({ length: 255 }).notNull().primaryKey().$defaultFn(() => crypto.randomUUID()),
-  busId: d.varchar({ length: 255 }).notNull().references(() => buses.id),
-  userId: d.varchar({ length: 255 }),
-  satus: d
-    .enum("available", "booked", "reserved")
+export const busesBusNumberRouteIdx = index("buses_bus_number_idx").on(
+  buses.busNumber,
+  buses.routeName,
+);
+
+export const busesRelations = relations(buses, ({ many }) => ({
+  seats: many(seats),
+  boardingPoints: many(busBoardingPoints),
+  users: many(users),
 }));
+
+export const seatStatusEnum = pgEnum("seatStatus", [
+  "available",
+  "booked",
+  "reserved",
+]);
+
+export const seats = createTable("seat", (d) => ({
+  id: d.varchar({ length: 255 }).notNull().primaryKey(),
+  userId: d
+    .varchar({ length: 255 })
+    .notNull()
+    .references(() => users.id),
+  busId: d
+    .varchar({ length: 255 })
+    .notNull()
+    .references(() => buses.id),
+  status: seatStatusEnum("status").default("available"),
+}));
+
+export const seatsRelations = relations(seats, ({ one }) => ({
+  bus: one(buses, { fields: [seats.busId], references: [buses.id] }),
+  user: one(users, { fields: [seats.userId], references: [users.id] }),
+}));
+
+export const userBusIdx = index("user_bus_idx").on(users.busId, users.id);
+
+export const boardingPoints = createTable("boardingPoint", (d) => ({
+  id: d
+    .varchar({ length: 255 })
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: d.varchar({ length: 255 }).notNull(),
+  latitude: d.doublePrecision(),
+  longitude: d.doublePrecision(),
+}));
+
+export const boardingPointsNameIdx = index("boarding_points_name_idx").on(
+  boardingPoints.name,
+);
+
+export const boardingPointsRelations = relations(
+  boardingPoints,
+  ({ many }) => ({
+    buses: many(busBoardingPoints),
+    acceptedRolls: many(acceptedRolls),
+    users: many(users),
+  }),
+);
+
+export const busBoardingPoints = createTable("busBoardingPoint", (d) => ({
+  id: d.serial().notNull().primaryKey(),
+  busId: d
+    .varchar({ length: 255 })
+    .notNull()
+    .references(() => buses.id),
+  boardingPointId: d
+    .varchar({ length: 255 })
+    .notNull()
+    .references(() => boardingPoints.id),
+  arrivalTime: d.timestamp({ mode: "date", withTimezone: true }),
+}));
+
+export const busBoardingPointsRelations = relations(
+  busBoardingPoints,
+  ({ one }) => ({
+    bus: one(buses, {
+      fields: [busBoardingPoints.busId],
+      references: [buses.id],
+    }),
+    boardingPoint: one(boardingPoints, {
+      fields: [busBoardingPoints.boardingPointId],
+      references: [boardingPoints.id],
+    }),
+  }),
+);
