0770e52fd9bc6bf60f0236d8cdbdcdd2964570b3
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-07-02 13:55:33 +0530

    fix: theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Title from "@/components/Title";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-background">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <Title />
+        <div className="absolute right-5 top-5">
+          <ThemeToggle />
+        </div>
       </div>
     </main>
   );
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title() {
   return (
-    <h1 className="text-5xl font-extrabold  text-primary-foreground sm:text-[5rem]">
+    <h1 className="text-5xl font-extrabold  text-card-foreground sm:text-[5rem]">
       <span className="text-primary">Bus</span>Mate
     </h1>
   );
