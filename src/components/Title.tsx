8ac267fccf9306ea05077c7d28be121d6123eaed
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-07-02 12:56:06 +0530

    add regiter page

---
+++ src/components/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+  return (
+    <h1 className="text-5xl font-extrabold  text-primary-foreground sm:text-[5rem]">
+      <span className="text-primary">Bus</span>Mate
+    </h1>
+  );
+}
