8042769f6e0fb69804ac29d4c5b9b02c8c8cb4f0
mohamed-faaris <mohammedfaaris2005@gmail.com>
2025-08-21 20:20:52 +0530

    Add error handling for bus seat fetching in Bus component

--- src/components/bus/bus.tsx
+++ src/components/bus/bus.tsx
@@ -19,10 +19,20 @@ export default function Bus({ busId }: { busId: string }) {
     queryKey: ["busSeats", busId],
     queryFn: () => fetch(`/api/bus/${busId}`).then((res) => res.json()),
   });
-  
-  return (
-    <>
-      <BusWrapper busId={busId} busSeats={busSeats.data.model.data} />
-    </>
-  );
+  try{
+    return (
+      <>
+        <BusWrapper busId={busId} busSeats={busSeats.data.model.data} />
+      </>
+    );
+
+  }
+  catch (error) {
+    console.error("Error fetching bus seats:", error);
+    return (
+      <>
+        <h2>Error fetching bus seats</h2>
+      </>
+    );
+  }
 }
